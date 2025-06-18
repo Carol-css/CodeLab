82db5266ce4af4dc210d680bcac875d0ab65f741
Carol Fernandes <carolfernandez543@gmail.com>
2025-06-19 00:24:50 +0530

    Update Login_signup.java

--- codelab/codelab/Login_signup.java
+++ codelab/codelab/Login_signup.java
@@ -68,7 +68,7 @@ public class Login_signup extends AppCompatActivity {
 
         // Configure Google Sign-In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)) // Get from Firebase settings
+                .requestIdToken(getString(R.string.default_web_client_id)) 
                 .requestEmail()
                 .build();
 
